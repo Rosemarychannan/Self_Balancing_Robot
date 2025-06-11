c025e2e2241b655bf707b4b19e41cf2f919bbc86
Nan Chan <148880520+Rosemarychannan@users.noreply.github.com>
2025-06-11 14:27:46 -0700

    Update Final_Product.ino

--- Final_Product/Final_Product.ino
+++ Final_Product/Final_Product.ino
@@ -9,9 +9,15 @@
 #define A 1
 #define B 2
 
+float k = 0.6;
+float kp = 1;
+float ki = 1;
+float kd = 1;
 float old_theta = 0; 
 float gyro_theta = 0;
 unsigned long start_time = 0;
+float desired_angle, error, ki, kp, kd;
+float old_error = 0;
 
 //Functions for motor controll
 void motorOff(int num){
@@ -25,26 +31,23 @@ void motorOff(int num){
     analogWrite(BIN2,255);
   }
 }
-void forward(int num, int pwm_percent){
-  int duty_cycle = 255.0f * pwm_percent / 100.0f;
-  int 
+void forward(int num, int pwm){
   if(num == A){
     analogWrite(AIN1,255);
-    analogWrite(AIN2,duty_cycle);
+    analogWrite(AIN2,pwm);
   }
   else if(num == B){
     analogWrite(BIN1,255);
-    analogWrite(BIN2,duty_cycle);
+    analogWrite(BIN2,pwm);
   }
 }
-void reverse(int num, int pwm_percent){
-  int duty_cycle = 255.0f * pwm_percent / 100.0f;
+void reverse(int num, int pwm){
   if(num == A){
-    analogWrite(AIN1,duty_cycle);
+    analogWrite(AIN1,pwm);
     analogWrite(AIN2,255);
   }
   else if(num == B){
-    analogWrite(BIN1,duty_cycle);
+    analogWrite(BIN1,pwm);
     analogWrite(BIN2,255);
   }
 }
@@ -54,23 +57,23 @@ void both_motorOff(){
   analogWrite(BIN1,255);
   analogWrite(BIN2,255);
 }
-void both_forward(int pwm_percent){
-  forward(A,pwm_percent);
-  forward(B,pwm_percent);
+void both_forward(int pwm){
+  forward(A,pwm);
+  forward(B,pwm);
 }
-void both_reverse(int pwm_percent){
-  reverse(A,pwm_percent);
-  reverse(B,pwm_percent);
+void both_reverse(int pwm){
+  reverse(A,pwm);
+  reverse(B,pwm);
 }
-void opposite_A(int pwm_percent){
-  int re_pwm_percent = 100 - pwm_percent;
-  forward(A,pwm_percent);
-  reverse(B,re_pwm_percent);
+void opposite_A(int pwm){
+  int re_pwm = 100 - pwm;
+  forward(A,pwm);
+  reverse(B,re_pwm);
 }
-void opposite_B(int pwm_percent){
-  int re_pwm_percent = 100 - pwm_percent;
-  forward(B,pwm_percent);
-  reverse(A,re_pwm_percent);
+void opposite_B(int pwm){
+  int re_pwm = 100 - pwm;
+  forward(B,pwm);
+  reverse(A,re_pwm);
 }
 void setup() {
   Serial.begin(9600);
@@ -94,56 +97,60 @@ void loop() {
   float acc_x, acc_y, acc_z;
   float gyro_x, gyro_y, gyro_z;
   float acc_theta, theta;
-  float k = 0.6;
+  int pwm;
 
-  int pwm_percent;
+  while(1){
+    //sensor measurement and angle calculation
+    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+      // Read accelerometer
+      IMU.readAcceleration(acc_x, acc_y, acc_z);
+      acc_theta = atan2(acc_y, acc_z) * 180.0 / M_PI;
 
-  //sensor measurement and angle calculation
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
-    // Read accelerometer
-    IMU.readAcceleration(acc_x, acc_y, acc_z);
-    acc_theta = atan2(acc_y, acc_z) * 180.0 / M_PI;
+      // Read gyroscope
+      IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
+      gyro_x = -gyro_x;  // Axis alignment
 
-    // Read gyroscope
-    IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
-    gyro_x = -gyro_x;  // Axis alignment
+      // Calculate delta time in seconds
+      unsigned long now = micros();
+      float dt = (now - start_time) / 1000000.0;
+      start_time = now;
 
-    // Calculate delta time in seconds
-    unsigned long now = micros();
-    float dt = (now - start_time) / 1000000.0;
-    start_time = now;
+      // Integrate gyro to get angle
+      gyro_theta += gyro_x * dt;
 
-    // Integrate gyro to get angle
-    gyro_theta += gyro_x * dt;
+      // Complementary filter
+      theta = (1 - k) * acc_theta + k * (old_theta + gyro_x * dt);
 
-    // Complementary filter
-    theta = (1 - k) * acc_theta + k * (old_theta + gyro_x * dt);
-    old_theta = theta;
-
-    // Print values
-    Serial.print(acc_theta);
-    Serial.print(" ");
-    Serial.print(gyro_theta);
-    Serial.print(" ");
-    Serial.println(theta);
-  }
-  //MAYBE WE SHOULD PUT ALL THE FOLLOWING IN THE IF STATEMENT ABOVE SO IT ONLY WORKS ON VALID ANGLES, NOT TOO SURE HERE
-  //calculating poritive percentage of PWM needed based on tilt angle
-  pwm_percent = 100.0 - 100.0*(90.0 - fabs(theta))/90.0; //fabs() finds absolute value
-  Serial.println(pwm_percent); //print value for debugging
-
-  // assuming positive means needs to go in reverse to bring back to zero here, and vise versa
-  if(pwm_percent <= 100 && pwm_percent >= 0){     //make sure not to send invalid data
-    if(pwm_percent == 0){
-        both_motorOff();
-      }
-    else{
-      if(theta < 0) both_forward(pwm_percent);  //use theta to determine sign (direction) since pwm_percent is always positive
-      else if(theta > 0) both_reverse(pwm_percent);
-      else both_motorOff();
+      // Print values
+      Serial.print(acc_theta);
+      Serial.print(" ");
+      Serial.print(gyro_theta);
+      Serial.print(" ");
+      Serial.println(theta);
     }
-  }
-  else{
-    Serial.println("Invalid Data");
+    //MAYBE WE SHOULD PUT ALL THE FOLLOWING IN THE IF STATEMENT ABOVE SO IT ONLY WORKS ON VALID ANGLES, NOT TOO SURE HERE
+    //calculating poritive percentage of PWM needed based on tilt angle
+    error = desired_angle - theta;
+    d_theta = (old_theta - theta)/dt;
+    i_theta += (error + old_error)/2*dt;
+    pid_out = (kp * error) + (ki * i_theta) + (kd * d_theta);
+    if(pid_out < -255 or pid_out > 255) pwm = 255;
+    Serial.println(pwm); //print value for debugging
+    
+    // assuming positive means needs to go in reverse to bring back to zero here, and vise versa
+    if(pwm <= 255 && pwm >= 0){     //make sure not to send invalid data
+      if(pwm == 0){
+          both_motorOff();
+        }
+      else{
+        if(pid_out < 0) both_forward(pwm);  //use theta to determine sign (direction) since pwm is always positive
+        else if(pid_out > 0) both_reverse(pwm);
+        else both_motorOff();
+      }
+    }
+    else{
+      Serial.println("Invalid Data");
+    }
+    old_theta = theta;
   }
 }
