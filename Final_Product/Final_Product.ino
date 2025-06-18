688e2d6bd1a7fe39b9d2346da6a0b4561165f5a9
Daniel Kim <danielkim2374@gmail.com>
2025-06-18 14:06:47 -0700

    jjjj

--- Final_Product/Final_Product.ino
+++ Final_Product/Final_Product.ino
@@ -1,7 +1,7 @@
 // A is left wheel and B is right wheel
 // AIN1 and BIN1 used for reverse
 // AIN2 and BIN2 used for forward
-// Comment
+
 
 #include <ArduinoBLE.h>
 #include "Arduino_BMI270_BMM150.h"
